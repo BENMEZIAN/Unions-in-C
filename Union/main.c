96fc31c29dd4d1d4008a313bd99337e032deb721
BENMEZIAN <44295887+BENMEZIAN@users.noreply.github.com>
2019-04-01 15:05:52 +0100

    Update main.c

--- Union/main.c
+++ Union/main.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[]) {
 	        printf("donner la valeur n:");
 
 	        do{
-	        	 scanf("%d",&n);
-			}while(n<0  ||  n>10);
+	           scanf("%d",&n);
+		}while(n<0  ||  n>10);
 
-			printf("1-le remplissage de tableau:\n");
+		printf("1-le remplissage de tableau:\n");
 	        remplissage(stud,n);
-			printf("\n");
-			printf("2-l'affichage de tableau:\n");
+		printf("\n");
+		printf("2-l'affichage de tableau:\n");
 	        afficher(stud,n);
 	     getch();
 
@@ -36,23 +36,23 @@ void remplissage(union student stud[],int n){
 	       int i;
 	      for(i=0;i<n;i++){
                  printf("les informations de l'etudiant numero:%d\n",i+1);
-	         	 printf("donner le nom d'etudiant:");
-	         	 scanf("%s",&stud[i].nom);
-				 printf("donner le prenom d'etudiant:");
-	             scanf("%s",&stud[i].prenom);
-	             printf("donner le matricule d'etudiant:");
-	         	 scanf("%d",&stud[i].matricule);
-	         	 printf("\n");
-				 }
+	         printf("donner le nom d'etudiant:");
+	         scanf("%s",&stud[i].nom);
+		 printf("donner le prenom d'etudiant:");
+	         scanf("%s",&stud[i].prenom);
+	         printf("donner le matricule d'etudiant:");
+	         scanf("%d",&stud[i].matricule);
+	         printf("\n");
+	       }
 }
 
 void afficher(union student stud[],int n){
 	       int i;
 	      for(i=0;i<n;i++){
                  printf("l'affichage des informatins l'etudiant numero:%d\n",i+1);
-	         	 printf("donner le nom d'etudiant:%s\n",stud[i].nom);
-	             printf("donner le prenom d'etudiant:%s\n",stud[i].prenom);
-	             printf("donner le matricule d'etudiant:%d\n",stud[i].matricule);
-	             printf("\n");
-				 }
+	         printf("donner le nom d'etudiant:%s\n",stud[i].nom);
+	         printf("donner le prenom d'etudiant:%s\n",stud[i].prenom);
+	         printf("donner le matricule d'etudiant:%d\n",stud[i].matricule);
+	         printf("\n");
+	      }
 }
